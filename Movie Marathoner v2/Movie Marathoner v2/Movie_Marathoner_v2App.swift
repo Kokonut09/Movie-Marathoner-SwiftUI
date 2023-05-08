fe7b6c7c16007f729c978026426dc1dce2801242
Andrew Saeyang <andrewsaeyang@pgatourhq.com>
2023-05-07 22:57:45 -0400

    Inital Commit with base files

---
+++ Movie Marathoner v2/Movie Marathoner v2/Movie_Marathoner_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Marathoner_v2App.swift
+//  Movie Marathoner v2
+//
+//  Created by Andrew Saeyang on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Marathoner_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
